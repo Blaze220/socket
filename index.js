669f32c473e938abc8168c1430ee05cf02ff675a
FIRS <Blazethecat220@gmail.com>
2023-12-04 21:46:29 +0600

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+// const PATH = require("path")
+
+// console.log("Склеить участки пути",PATH.join(__dirname,"ssss","..","wwwww",))
+// let fullPath = ("Получить обсалютный путь",PATH.resolve("eefeffe","ssss","..","wwwww.txt",))
+
+// console.log("парсинг пути", PATH.parse(fullPath))
+// console.log("разделитель в ос", PATH.sep)
+// console.log("Проверка на обсалютный путь", PATH.isAbsolute(fullPath))
+// console.log("Проверка на обсалютный путь", PATH.isAbsolute("oiii"))
+// console.log("Название файла",PATH.basename(fullPath))
+// console.log("засширение файла",PATH.extname(fullPath))
+
+
+// const siteURL = "http://localhost:8080/users?id=123"
+
+// const url = new URL(siteURL)
+
+// console.log(url)
